544f7867af07d7ef1eb8cd758e5792b64d387ce4
Prismate <118563956+Prismate@users.noreply.github.com>
2023-01-22 09:10:34 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,6 +7,7 @@ POSSIBLE_OPTIONS = [1, 2, 3, 4]
 def clear_screen():
     os.system('clear')
 
+    
 def get_user_input():
     try:
         user_input = int(input('Please enter a number from 1 to 4: '))
@@ -14,21 +15,12 @@ def get_user_input():
     except:
         return None
 
+    
 def print_menu():
     print('1. Human vs Human\n2. Random AI vs Random AI\n3. Human vs Random AI\n4. Human vs Unbeatable AI')
 
+    
 def get_menu_option():
-    '''
-    Should print a menu with the following options:
-    1. Human vs Human
-    2. Random AI vs Random AI
-    3. Human vs Random AI
-    4. Human vs Unbeatable AI
-
-    The function should return a number between 1-4.
-    If the user will enter invalid data (for example 5), than a message will appear
-    asking to input a new value.
-    '''
     while True:
         print_menu()
         user_input = get_user_input()
